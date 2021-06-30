f33c6347fa9b28e8f4ed75408dcd7e1a99bfca4c
Yurii Lisovych <yurii.lisovych.se@gmail.com>
2021-06-30 17:20:39 +0300

    implement second function

--- script.js
+++ script.js
@@ -5,4 +5,25 @@ const getArray = (length, min, max) => {
     newArray.push(Math.floor(Math.random() * (max - min + 1) + min));
   }   return newArray;
 }
+// (2 function) get Moda
+const getModa = (...numbers) => {
+  let mostFrequentlyDigit;
+  let max = 0;
 
+  numbers.forEach((number) => {
+    let frequencyOfCurrentDigit = getDigitFrequencyInNumbers(numbers, number);
+
+    if (frequencyOfCurrentDigit > max) {
+      max = frequencyOfCurrentDigit;
+      mostFrequentlyDigit = number;
+    }
+  });
+
+  return mostFrequentlyDigit;
+}
+
+const getDigitFrequencyInNumbers = (numbers, numberToFind) => {
+  return numbers.filter(number => number === numberToFind).length;
+}
+
+console.log("Moda ", getModa(6, 2, 55, 11, 78, 2, 55, 77, 57, 87, 23, 2, 56, 3, 2));
