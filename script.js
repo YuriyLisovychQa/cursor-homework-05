a6090762607a37e90ec931e5e27cea441bb457a0
Yurii Lisovych <yurii.lisovych.se@gmail.com>
2021-07-11 10:34:51 +0300

    code style fix

--- script.js
+++ script.js
@@ -3,17 +3,18 @@ const filterIntegerNumber = (array) => {
 }
 
 //(1 function) get array
+
 const getArray = (length, min, max) => {
-  let newArray =[]
-  for (let i = 0; i < length; i++) {
-    newArray.push(Math.floor(Math.random() * (max - min + 1) + min));
-  }   return newArray;
+  return Array.from({length})
+      .map(el => getRandomNumber(min, max));
 }
 
-console.log("Get random array ", getArray(15, 1, 15));
+const getRandomNumber = (min, max) => {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
 
 // (2 function) get Moda
-const getModa = (...numbers) => {
+const getModa = (numbers) => {
   let filteredArray = filterIntegerNumber(numbers);
   let mostFrequentlyDigit;
   let max = 0;
@@ -34,53 +35,54 @@ const getDigitFrequencyInNumbers = (numbers, numberToFind) => {
   return numbers.filter(number => number === numberToFind).length;
 }
 
-console.log("Moda ", getModa(6, 2, 55, 11, 78, 2, 55, 77, 57, 87, 23, 2, 56, 3, 2));
-
 //(3 function) get average
-const getAverage = (...numbers) => {
+const getAverage = (numbers) => {
   let filteredArray = filterIntegerNumber(numbers);
   let sum = 0;
 
   filteredArray.forEach((number) => sum += number);
 
-  return sum / filteredArray.length;
+  return Number((sum / filteredArray.length).toFixed(2));
 }
 
-console.log("Average value", getAverage(6, 2, 55, 11, 78, 2, 55, 77, 57, 87, 23, 2, 56, 3, 2));
-
 //(4 function) get Median
-const getMedian = (...numbers) => {
+const getMedian = (numbers) => {
   const filteredArray = filterIntegerNumber(numbers);
   const sortedArray = filteredArray.sort((a, b) => a - b);
   const indexOfMiddleElement = sortedArray.length / 2;
 
   return Number.isInteger(indexOfMiddleElement)
-      ? sortedArray[
-          (sortedArray[indexOfMiddleElement] +
+      ? (sortedArray[indexOfMiddleElement] +
           sortedArray[indexOfMiddleElement - 1]) / 2
-        ]
       : sortedArray[Math.floor(indexOfMiddleElement)];
 };
 
-console.log("Median of array", getMedian(6, 2, 55, 11, 78, 2, 55, 77, 57, 87, 23, 2, 56, 3, 2));
-
 //(5 function) get odd numbers
-const filterEvenNumbers = (...numbers) => {
+const filterEvenNumbers = (numbers) => {
   return numbers.filter(number => number % 2);
 }
-
-console.log("Odd numbers", filterEvenNumbers(6, 2, 55, 11, 78, 2, 55, 77, 57, 87, 23, 2, 56, 3, 2));
 
 //(6 function) Positive numbers
 const countPositiveNumbers = (...numbers) => {
   return numbers.filter(number => number > 0).length;
 }
 
-console.log("Count positive numbers", countPositiveNumbers(1, -2, 3, -4, -5, 6));
-
 //(7 function) Divided by 5
-const getDividedByFive = (...numbers) => {
+const getDividedByFive = (numbers) => {
   return numbers.filter(number => Number.isInteger(number / 5)).length;
 }
 
-console.log("Count divided numbers by five", getDividedByFive(6, 2, 55, 11, 78, 2, 55, 77, 57, 87, 23, 2, 56, 3, 2));
+const arr = getArray(15, 1, 15);
+
+console.log("Random numbers: ", arr);
+console.log("Moda: ", getModa(arr));
+
+console.log("Average value: ", getAverage(arr));
+
+console.log("Median of array: ", getMedian(arr));
+
+console.log("Odd numbers: ", filterEvenNumbers(arr));
+
+console.log("Count positive numbers: ", countPositiveNumbers(1, -2, 3, -4, -5, 6));
+
+console.log("Count divided numbers by five: ", getDividedByFive(arr));
